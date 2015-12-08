9aacd446d32ff95ca5e15673946f12e66deee3b9
egoist <0x142857@gmail.com>
2015-12-08 14:26:47 +0800

    fix boolean value

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = function (argv, command) {
   argv = argv || {};
   command = command ? command + ' ' : '';
-  var input = argv._;
+  var input = argv._ || [];
   input = input.join(' ');
   var flags = '';
   for (var i in argv) {
     if (i !== '_') {
-      flags += ' --' + i + ' ' + argv[i];
+      var value = Boolean(argv[i]) === argv[i] ? '' : ' ' + argv[i];
+      flags += ' --' + i + value;
     }
   }
   return command + input + flags;
